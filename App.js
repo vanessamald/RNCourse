4d87d9ce31d6475a0b8fe86dae37989336d1c31d
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2023-07-14 12:07:45 -0500

    adding ScrollView for goal list

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
   // entered goal
@@ -25,11 +25,13 @@ export default function App() {
         <Button title='Add Goal' onPress={addGoal}></Button>
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal)=> (
+        <ScrollView>
+          {courseGoals.map((goal)=> (
           <View key={goal} style={styles.goalItem}>
             <Text style={styles.goalText}>{goal}</Text>
           </View>
         ))}
+        </ScrollView>
       </View>
     </View>
   );
